7673dea0f57b51a920ef230da200055ef51d3192
Martin Johnson <M.J.Johnson@massey.ac.nz>
2012-02-19 08:55:22 +1300

    Dont use early_suspend for gpu, this causes it to crash sometimes when the screen is turned off. Shouldn't affect power consumption much.

--- drivers/gpu/msm/kgsl_pwrctrl.c
+++ drivers/gpu/msm/kgsl_pwrctrl.c
@@ -545,7 +545,7 @@ int kgsl_pwrctrl_init(struct kgsl_device *device)
 		goto done;
 	}
 
-	register_early_suspend(&device->display_off);
+//	register_early_suspend(&device->display_off);
 	return result;
 
 clk_err:
@@ -564,7 +564,7 @@ void kgsl_pwrctrl_close(struct kgsl_device *device)
 
 	KGSL_PWR_INFO(device, "close device %d\n", device->id);
 
-	unregister_early_suspend(&device->display_off);
+//	unregister_early_suspend(&device->display_off);
 
 	if (pwr->interrupt_num > 0) {
 		if (pwr->have_irq) {
